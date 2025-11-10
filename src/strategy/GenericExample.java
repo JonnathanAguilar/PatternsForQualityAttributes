592438b00c8e91d7e076719e79bd705e2294ad56
wiib <s.caezsiguenza@gmail.com>
2025-11-09 22:07:49 -0500

    Add generic example
    This example is only for reference, and doesn't actually work.

---
+++ src/strategy/GenericExample.java
@@ -0,0 +1,62 @@
+package strategy;
+
+class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Route {
+}
+
+interface RouteStrategy {
+    Route buildRoute(Point a, Point b);
+}
+
+class BusStrategy implements RouteStrategy {
+    @Override
+    public Route buildRoute(Point a, Point b) {
+        // Implementación específica para buses...
+        return new Route();
+    }
+}
+
+class WalkingStrategy implements RouteStrategy {
+    @Override
+    public Route buildRoute(Point a, Point b) {
+        // Implementación específica para caminatas...
+        return new Route();
+    }
+}
+
+class Navigator {
+    private RouteStrategy routeStrategy;
+
+    public Route buildRoute(Point a, Point b) {
+        return routeStrategy.buildRoute(a, b);
+    }
+
+    public void setStrategy(RouteStrategy s) {
+        this.routeStrategy = s;
+    }
+}
+
+class Client {
+    public static void main(String[] args) {
+        Point a = new Point(1, 1);
+        Point b = new Point(5, 2);
+
+        // El cliente conoce la estrategia a utilizar
+        Navigator nav = new Navigator();
+
+        nav.setStrategy(new BusStrategy());
+        Route busRoute = nav.buildRoute(a, b);
+
+        nav.setStrategy(new WalkingStrategy());
+        Route walkingRoute = nav.buildRoute(a, b);
+    }
+}
